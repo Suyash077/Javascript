da352ea8a60ccd0136396aae259bd6a59d5c7f68
Suyash077 <111738055+Suyash077@users.noreply.github.com>
2024-05-14 09:17:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //Treat all js code as newer version
+
+//alert(3+3) //we are using nodejs not browser
+
+console.log(3+3)
+console.log("Suyash")
+
+let name="Suyash"
+let age=21
+let isLoggedIn=false
+
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+
+console.log(typeof null);
+console.log(typeof undefined);
+//Type of null is object while type of undefined is undefined
